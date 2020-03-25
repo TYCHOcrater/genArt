b4d1ea28cdc46bba4ad4843cc9dced275de42e90
TYCHOcrater <paketpaper@gmail.com>
2020-03-25 16:41:42 +0200

    Updating Some Settings

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048,2048],
+  // orientation: 'landscape',
+  // units: 'cm',
+  // pixelsPerInch: 300
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
+    console.log(width, height);
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
+    //beginPath() -> creating a circle primitives
     context.beginPath();
-    context.arc(width / 2, height / 2, 400, 0, Math.PI * 4.5, false);
+    context.arc(width / 2, height / 2, 2, 0, Math.PI * 4.5, false);
     context.fillStyle = 'red';
     context.fill();
-    context.lineWidth = 20;
+
+    //strokes outlines for primitives after beginPath()
+    context.lineWidth = width * 0.02;
     context.strokeStyle = 'blue';
     context.stroke();
   };
