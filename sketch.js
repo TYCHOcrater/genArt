14e7294a1cd14ee80d5b1b47b8993db8530fd15b
TYCHOcrater <paketpaper@gmail.com>
2020-03-25 18:19:04 +0200

    Gaussian Curve

--- sketch.js
+++ sketch.js
@@ -14,7 +14,10 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([ u, v ]);
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.005),
+          position: [ u, v ]
+        });
       }
     }
     return points;
@@ -25,18 +28,25 @@ const sketch = () => {
   const margin = 400;
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
     
-    points.forEach(([ u, v ]) => {
+    points.forEach(data => {
+      const {
+        position,
+        radius
+      } = data;
+
+      const [ u, v ] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.strokeStyle = 'black';
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = 'red';
       context.lineWidth = 20;
-      context.stroke();
+      context.fill();
     });
   };
 };
