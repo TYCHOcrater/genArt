6b98895b548164b15a7c292bd778bd1cbd4ed761
TYCHOcrater <paketpaper@gmail.com>
2020-03-29 21:27:27 +0300

    FragColor Vec4 Vec3

--- shader.js
+++ shader.js
@@ -16,8 +16,7 @@ const frag = glsl(`
   varying vec2 vUv;
 
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(vec3(vUv.x), 1.0);
   }
 `);
 
