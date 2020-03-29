0f7bad362d32724774bd70a5e0d0fedeecface27
TYCHOcrater <paketpaper@gmail.com>
2020-03-29 21:46:58 +0300

    Smoothstep

--- shader.js
+++ shader.js
@@ -13,14 +13,21 @@ const frag = glsl(`
   precision highp float;
 
   uniform float time;
+  uniform float aspect;
   varying vec2 vUv;
 
   void main () {
     vec3 colorA = vec3(1.0, 0.0, 0.0);
     vec3 colorB = vec3(0.0, 1.0, 0.0);
 
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+    float dist = length(center);
+
+    float alpha = smoothstep(0.2515, 0.25, dist);
+
     vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, alpha);
   }
 `);
 
@@ -28,6 +35,7 @@ const frag = glsl(`
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
+    clearColor: 'white',
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
@@ -35,7 +43,8 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time
+      time: ({ time }) => time,
+      aspect: ({ width, height }) => width / height
     }
   });
 };
