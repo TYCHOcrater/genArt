09a6afa495281536f8744c2afd811afdc39f63e6
TYCHOcrater <paketpaper@gmail.com>
2020-03-29 21:32:17 +0300

    Gradient mix

--- shader.js
+++ shader.js
@@ -16,7 +16,11 @@ const frag = glsl(`
   varying vec2 vUv;
 
   void main () {
-    gl_FragColor = vec4(vec3(vUv.x), 1.0);
+    vec3 colorA = vec3(1.0, 0.0, 0.0);
+    vec3 colorB = vec3(0.0, 1.0, 0.0);
+
+    vec3 color = mix(colorA, colorB, vUv.x);
+    gl_FragColor = vec4(color, 1.0);
   }
 `);
 
