edbf566bb7e6d3f888feeee1fedd179e4473ab0e
TYCHOcrater <paketpaper@gmail.com>
2020-03-29 23:22:35 +0300

    Noise

--- shader.js
+++ shader.js
@@ -16,18 +16,24 @@ const frag = glsl(`
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
   void main () {
-    vec3 colorA = cos(time) + vec3(1.0, 0.0, 0.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.0);
+    // vec3 colorA = cos(time) + vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 1.0, 0.0);
 
-    vec2 center = vUv - 0.5;
-    center.x *= aspect;
-    float dist = length(center);
+    // vec2 center = vUv - 0.5;
+    // center.x *= aspect;
+    // float dist = length(center);
 
-    float alpha = smoothstep(0.2515, 0.25, dist);
+    // float alpha = smoothstep(0.2515, 0.25, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.x + vUv.y * sin(time));
-    gl_FragColor = vec4(color, alpha);
+    // vec3 color = mix(colorA, colorB, vUv.x + vUv.y * sin(time));
+    // gl_FragColor = vec4(color, alpha);
+
+    float n = noise(vec3(vUv.xy, time));
+
+    gl_FragColor = vec4(vec3(n), 1.0);
   }
 `);
 
